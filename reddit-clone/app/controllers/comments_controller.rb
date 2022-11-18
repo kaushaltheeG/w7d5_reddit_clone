abe9df855c36fae2449ac2f0c29103ccab2e9835
matthew <matthewgoodbar@gmail.com>
2022-11-18 15:17:24 -0800

    Implement comments controller, new form for comments

--- reddit-clone/app/controllers/comments_controller.rb
+++ reddit-clone/app/controllers/comments_controller.rb
@@ -8,8 +8,8 @@ class CommentsController < ApplicationController
     def create 
         @comment = Comment.new(comment_params)
         @comment.author_id = current_user.id 
-        @comment.post_id = params[:post_id]
-
+        # @comment.post_id = params[:post_id]
+        debugger
         if @comment.save 
             redirect_to post_url(@comment.post_id)
         else 
@@ -18,18 +18,18 @@ class CommentsController < ApplicationController
         end 
     end 
 
-    def show 
-        @comment = Comment.find_by(id: params[:id])
-        if @comment
-            render :show 
-        else
-            redirect_to subs_url  
-        end 
-    end 
+    # def show 
+    #     @comment = Comment.find_by(id: params[:id])
+    #     if @comment
+    #         render :show 
+    #     else
+    #         redirect_to subs_url  
+    #     end 
+    # end 
 
     private 
 
     def comment_params
-        params.require(:comment).permit(:body)
+        params.require(:comment).permit(:body, :post_id)
     end 
 end
